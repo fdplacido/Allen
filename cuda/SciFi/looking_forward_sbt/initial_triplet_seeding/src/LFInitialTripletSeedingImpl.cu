b08be17604e80badafd732761d6cdc8d44b5f0d9
Daniel Campora <dcampora@cern.ch>
2019-03-19 00:02:37 +0100

    Implemented Looking Forward seeding in TCs
    * Looking Forward seeding is now implemented
      using Tensor cores. It is significantly sped up
      when compiled / run with sm 70 or higher.
    * Default compilation option for CUDA_ARCH is now MAX.

--- cuda/SciFi/looking_forward_sbt/initial_triplet_seeding/src/LFInitialTripletSeedingImpl.cu
+++ cuda/SciFi/looking_forward_sbt/initial_triplet_seeding/src/LFInitialTripletSeedingImpl.cu
@@ -1,5 +1,6 @@
 #include "LFInitialTripletSeedingImpl.cuh"
 #include "BinarySearchTools.cuh"
+#include <mma.h>
 
 __device__ void lf_initial_triplet_seeding_impl(
   const float* scifi_hits_x0,
@@ -31,22 +32,108 @@ __device__ void lf_initial_triplet_seeding_impl(
   const float zdiff = dz2 / dz1;
   const float extrap2 = LookingForward::forward_param * qop * dz2 * dz2;
 
+#if __CUDA_ARCH__ >= 700
+  const half zdiff_half = ((half) zdiff);
+  const half big_max_chi2 = 1000.f * max_chi2;
+
+  // Tensor core magic
+  __shared__ half shared_wmma_a[tile_size * tile_size];
+  __shared__ half shared_wmma_b[tile_size * tile_size];
+  nvcuda::wmma::fragment<nvcuda::wmma::matrix_a, tile_size, tile_size, tile_size, half, nvcuda::wmma::col_major> a_frag;
+  nvcuda::wmma::fragment<nvcuda::wmma::matrix_b, tile_size, tile_size, tile_size, half, nvcuda::wmma::row_major> b_frag;
+  nvcuda::wmma::fragment<nvcuda::wmma::accumulator, tile_size, tile_size, tile_size, float> c_frag;
+  nvcuda::wmma::fragment<nvcuda::wmma::accumulator, tile_size, tile_size, tile_size, float> d_frag;
+  // Initialize wmma shared memory arrays
+  for (int16_t k = threadIdx.x; k < tile_size * tile_size; k += blockDim.x) {
+    shared_wmma_a[k] = 0;
+    shared_wmma_a[k] = 0;
+  }
+  for (int16_t k = threadIdx.x; k < tile_size; k += blockDim.x) {
+    shared_wmma_a[k] = 1;
+    shared_wmma_b[tile_size + k] = 1;
+    shared_wmma_b[2 * tile_size + k] = zdiff_half;
+  }
+  nvcuda::wmma::fill_fragment(c_frag, -extrap2);
+
   // Search best triplet
   // Tiled processing of h0 and h2
   for (int8_t i = 0; i < (h0_candidate_size + tile_size - 1) >> tile_size_shift_div; ++i) {
     // Note: Part of wmma can be allocated here
+    for (int16_t k = threadIdx.x; k < tile_size; k += blockDim.x) {
+      const int8_t h0_rel = i * tile_size + k;
+      if (h0_rel < h0_candidate_size) {
+        const half x0 = scifi_hits_x0[scifi_lf_candidates[(relative_middle_layer - 1) * LookingForward::maximum_number_of_candidates + h0_rel]];
+        shared_wmma_a[tile_size + k] = -x0;
+        shared_wmma_a[2 * tile_size + k] = x0;
+      } else {
+        shared_wmma_a[tile_size + k] = big_max_chi2;
+        shared_wmma_a[2 * tile_size + k] = big_max_chi2;
+      }
+    }
+    // TODO: Needed?
+    __syncthreads();
+    nvcuda::wmma::load_matrix_sync(a_frag, shared_wmma_a, tile_size);
 
     for (int8_t j = 0; j < (h2_candidate_size + tile_size - 1) >> tile_size_shift_div; ++j) {
       // Note: The other part of wmma can be allocated here
+      for (int16_t k = threadIdx.x; k < tile_size; k += blockDim.x) {
+        const int8_t h2_rel = j * tile_size + k;
+        if (h2_rel < h2_candidate_size) {
+          shared_wmma_b[k] = scifi_hits_x0[scifi_lf_candidates[(relative_middle_layer + 1) * LookingForward::maximum_number_of_candidates + h2_rel]];
+        } else {
+          shared_wmma_b[k] = big_max_chi2;
+        }
+      }
+      // TODO: Needed?
+      __syncthreads();
+      nvcuda::wmma::load_matrix_sync(b_frag, shared_wmma_b, tile_size);
+
+      // Magic :)
+      nvcuda::wmma::mma_sync(d_frag, a_frag, b_frag, c_frag);
+      nvcuda::wmma::store_matrix_sync(shared_partial_chi2, d_frag, tile_size, nvcuda::wmma::mem_col_major);
+
+      // Iterate over all h1s
+      // Find best chi2, h0 and h2 using the partial chi2 from before
+      for (int16_t h1_rel = threadIdx.x; h1_rel < h1_candidate_size; h1_rel += blockDim.x) {
+        const float x1_zdiff = scifi_hits_x0[scifi_lf_candidates[relative_middle_layer * LookingForward::maximum_number_of_candidates + h1_rel]] 
+          * zdiff;
+
+        float local_best_chi2 = max_chi2;
+        int16_t local_best_k = -1;
+
+        for (int16_t k = 0; k < tile_size * tile_size; ++k) {
+          float chi2 = shared_partial_chi2[k] - x1_zdiff;
+          chi2 = extrap1 + chi2 * chi2;
+
+          if (chi2 < local_best_chi2) {
+            local_best_chi2 = chi2;
+            local_best_k = k;
+          }
+        }
+
+        if (local_best_k != -1 && local_best_chi2 < best_chi2[h1_rel]) {
+          best_chi2[h1_rel] = local_best_chi2;
+          best_h0_h2[h1_rel] = i * tile_size + (local_best_k & tile_size_mask);
+          best_h0_h2[LookingForward::maximum_number_of_candidates + h1_rel] = j * tile_size + (local_best_k >> tile_size_shift_div);
+        }
+      }
+    }
+  }
+  
+#else
+
+  // Search best triplet
+  // Tiled processing of h0 and h2
+  for (int8_t i = 0; i < (h0_candidate_size + tile_size - 1) >> tile_size_shift_div; ++i) {
+    for (int8_t j = 0; j < (h2_candidate_size + tile_size - 1) >> tile_size_shift_div; ++j) {
 
       __syncthreads();
 
-      // Note: This block of code is doable by wmma
       for (int16_t k = threadIdx.x; k < tile_size * tile_size; k += blockDim.x) {
         const int8_t h0_rel = i * tile_size + (k & tile_size_mask);
         const int8_t h2_rel = j * tile_size + (k >> tile_size_shift_div);
 
-        auto partial_chi2 = 1000.f * max_chi2;
+        float partial_chi2 = 1000.f * max_chi2;
         if (h0_rel < h0_candidate_size && h2_rel < h2_candidate_size) {
           const auto x0 = scifi_hits_x0[scifi_lf_candidates[(relative_middle_layer - 1) * LookingForward::maximum_number_of_candidates + h0_rel]];
           const auto x2 = scifi_hits_x0[scifi_lf_candidates[(relative_middle_layer + 1) * LookingForward::maximum_number_of_candidates + h2_rel]];
@@ -86,4 +173,5 @@ __device__ void lf_initial_triplet_seeding_impl(
       }
     }
   }
+#endif
 }
