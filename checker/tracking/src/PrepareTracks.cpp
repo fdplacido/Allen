53759522905c6bbeffb0ecb33ddf3c5a94aecb5a
Daniel Campora <dcampora@cern.ch>
2019-03-21 10:35:27 +0100

    Added back velo track hits LHCb IDs.

--- checker/tracking/src/PrepareTracks.cpp
+++ checker/tracking/src/PrepareTracks.cpp
@@ -32,6 +32,10 @@ std::vector<trackChecker::Tracks> prepareVeloTracks(
 
       const uint velo_track_number_of_hits = velo_tracks.number_of_hits(i_track);
       Velo::Consolidated::Hits velo_track_hits = velo_tracks.get_hits((char*) track_hits, i_track);
+
+      for (int i_hit = 0; i_hit < velo_track_number_of_hits; ++i_hit) {
+        t.addId(velo_track_hits.LHCbID[i_hit]);
+      }
       tracks.push_back(t);
     } // tracks
     checker_tracks.emplace_back(tracks);
