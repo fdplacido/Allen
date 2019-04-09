4cd27b0d8d8e7a6172ed72edb4d16b637849cc0d
Florian Reiss <freiss@cern.ch>
2019-04-09 15:45:29 +0200

    parallelize pv_cleanup

--- cuda/PV/beamlinePV/src/pv_beamline_cleanup.cu
+++ cuda/PV/beamlinePV/src/pv_beamline_cleanup.cu
@@ -7,39 +7,36 @@ __global__ void pv_beamline_cleanup(
   uint* dev_number_of_multi_final_vertices)
 {
 
+  __shared__ uint tmp_number_vertices[1];
+  *tmp_number_vertices = 0;
+
   const uint event_number = blockIdx.x;
 
   PV::Vertex* vertices = dev_multi_fit_vertices + event_number * PV::max_number_vertices;
   PV::Vertex* final_vertices = dev_multi_final_vertices + event_number * PV::max_number_vertices;
   uint* number_of_multi_fit_vertices = dev_number_of_multi_fit_vertices + event_number;
-  if (threadIdx.x == 0) {
-    int tmp_number_vertices = 0;
-
-    // loop over all rec PVs, check if another one is within certain sigma range, only fill if not
-    for (int i_pv = 0; i_pv < number_of_multi_fit_vertices[0]; i_pv++) {
-
-      bool unique = true;
-      PV::Vertex vertex1 = vertices[i_pv];
-      // PVs with such a small z uncertainty are very likely fakes
-      if (vertex1.cov22 > 100.f) continue;
-      if (vertex1.cov22 < 0.000000001f) continue;
-      for (int j_pv = 0; j_pv < tmp_number_vertices; j_pv++) {
-        PV::Vertex vertex2 = final_vertices[j_pv];
-        float z1 = vertex1.position.z;
-        float z2 = vertex2.position.z;
-        float variance1 = vertex1.cov22;
-        float variance2 = vertex2.cov22;
-        float chi2_dist = (z1 - z2) * (z1 - z2);
-        chi2_dist = chi2_dist / (variance1 + variance2);
-        if (chi2_dist < minChi2Dist || (z1 - z2) * (z1 - z2) < 1.f) {
-          unique = false;
-        }
-      }
-      if (unique) {
-        final_vertices[tmp_number_vertices] = vertex1;
-        tmp_number_vertices++;
+  // loop over all rec PVs, check if another one is within certain sigma range, only fill if not
+  for (int i_pv = threadIdx.x; i_pv < number_of_multi_fit_vertices[0]; i_pv += blockDim.x) {
+    bool unique = true;
+    PV::Vertex vertex1 = vertices[i_pv];
+    for (int j_pv = 0; j_pv < number_of_multi_fit_vertices[0]; j_pv++) {
+      if(i_pv == j_pv) continue;
+      PV::Vertex vertex2 = vertices[j_pv];
+      float z1 = vertex1.position.z;
+      float z2 = vertex2.position.z;
+      float variance1 = vertex1.cov22;
+      float variance2 = vertex2.cov22;
+      float chi2_dist = (z1 - z2) * (z1 - z2);
+      chi2_dist = chi2_dist / (variance1 + variance2);
+      if (chi2_dist < minChi2Dist  && vertex1.n_tracks < vertex2.n_tracks) {
+        unique = false;
       }
     }
-    dev_number_of_multi_final_vertices[event_number] = tmp_number_vertices;
+    if (unique) {
+      auto vtx_index = atomicInc(tmp_number_vertices, 100);
+      final_vertices[vtx_index] = vertex1;
+    }
   }
+  __syncthreads();
+  dev_number_of_multi_final_vertices[event_number] = *tmp_number_vertices;
 }
