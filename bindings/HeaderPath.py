1dbc702d9168c4f48c7565c6e23e7253f5d311a1
Roel Aaij <roel.aaij@cern.ch>
2019-04-30 14:02:54 +0200

    Add copyright statement

--- bindings/HeaderPath.py
+++ bindings/HeaderPath.py
@@ -1,6 +1,17 @@
+###############################################################################
+# (c) Copyright 2000-2018 CERN for the benefit of the LHCb Collaboration      #
+#                                                                             #
+# This software is distributed under the terms of the GNU General Public      #
+# Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING".   #
+#                                                                             #
+# In applying this licence, CERN does not waive the privileges and immunities #
+# granted to it by virtue of its status as an Intergovernmental Organization  #
+# or submit itself to any jurisdiction.                                       #
+###############################################################################
 import os
 import inspect
-# Hack until we sort out the procedure for a runtime environment
+
+# Dirty hack until we sort out the procedure for a runtime environment
 script_path = os.path.abspath(inspect.stack()[0][1])
 allen_dir = os.sep.join(script_path.split(os.sep)[:-2])
 root_include = os.environ['ROOT_INCLUDE_PATH'].split(':')
