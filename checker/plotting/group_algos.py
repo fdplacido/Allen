fbeefb5dca06475c9f9b51d92111283dafc28ca6
Daniel Campora <dcampora@cern.ch>
2019-05-18 12:23:52 +0200

    Updated group algos script.

--- checker/plotting/group_algos.py
+++ checker/plotting/group_algos.py
@@ -16,12 +16,14 @@ def group_algos(algorithm_times):
     velo_algorithms = ["consolidate_velo_tracks", "copy_velo_track_hit_number", "estimate_input_size", "masked_velo_clustering", "calculate_phi_and_sort", "search_by_triplet", "fill_candidates", "weak_tracks_adder", "copy_and_prefix_sum_single_block"]
     pv_algorithms = ["pv_beamline_peak", "pv_beamline_multi_fitter", "pv_beamline_histo", "pv_beamline_extrapolate"]
     ut_algorithms = ["consolidate_ut_tracks", "copy_ut_track_hit_number", "ut_decode_raw_banks_in_order", "ut_pre_decode", "ut_find_permutation", "ut_calculate_number_of_hits", "compass_ut", "ut_search_windows"]
-    scifi_algorithms = ["scifi_pre_decode_v4", "scifi_raw_bank_decoder_v4", "scifi_calculate_cluster_count_v4", "scifi_direct_decoder_v4", "consolidate_scifi_tracks", "copy_scifi_track_hit_number"]
+    scifi_algorithms = ["scifi_pre_decode_v4", "scifi_raw_bank_decoder_v4", "scifi_calculate_cluster_count_v4", "scifi_direct_decoder_v4", "consolidate_scifi_tracks", "copy_scifi_track_hit_number", \
+        "lf_search_initial_windows", "lf_collect_candidates", "lf_prefix_sum_candidates", "lf_triplet_seeding", "lf_triplet_keep_best", "lf_extend_tracks_x",  \
+        "lf_quality_filter_x", "lf_search_uv_windows", "lf_extend_tracks_uv", "lf_quality_filter_length", "lf_fit", "lf_quality_filter"]
     kalman_algorithms = ["velo_filter", "velo_kalman_fit"]
-
+    
     timings = dict([(frozenset(velo_algorithms), 0), (frozenset(pv_algorithms), 0), \
         (frozenset(ut_algorithms), 0), (frozenset(scifi_algorithms), 0), \
-        (frozenset(kalman_algorithms), 0), ("Common", 0)])
+        (frozenset(kalman_algorithms), 0), (frozenset(["Common"]), 0)])
 
     full_addition = sum(algorithm_times.values())
     for algo, value in algorithm_times.items():
@@ -32,7 +34,7 @@ def group_algos(algorithm_times):
                 found = True
                 break
         if not found:
-            timings["Common"] += 100 * value / full_addition
+            timings[frozenset(["Common"])] += 100 * value / full_addition
 
     timings = getTimingLabels(timings)
     output_list = sorted(timings.items(), key=operator.itemgetter(1), reverse=True)
