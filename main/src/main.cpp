e088cff398850f4ae13f3f008cee7a8d946361d1
Daniel Campora <dcampora@cern.ch>
2019-05-22 17:29:25 -0300

    Reworked input options.

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -7,97 +7,137 @@
  *      Started development on February, 2018
  *      CERN
  */
-#include <string>
-#include <map>
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <exception>
-#include <fstream>
-#include <cstdlib>
-#include <stdio.h>
-#include <unistd.h>
 #include <getopt.h>
-
+#include <cstring>
+#include <map>
+#include <string>
+#include <iostream>
+#include <vector>
 #include <Allen.h>
 #include <Updater.h>
 
-void printUsage(char* argv[])
-{
-  std::cerr << "Usage: " << argv[0] << std::endl
-            << " -f {folder containing data directories}=\"../input/minbias/\"" << std::endl
-            << " -g {folder containing detector configuration}" << std::endl
-            << " --mdf {use MDF files as input instead of binary files}" << std::endl
-            << " -n {number of events to process}=0 (all)" << std::endl
-            << " -o {offset of events from which to start}=0 (beginning)" << std::endl
-            << " -t {number of threads / streams}=1" << std::endl
-            << " -r {number of repetitions per thread / stream}=1" << std::endl
-            << " -c {run checkers}=1" << std::endl
-            << " -m {reserve Megabytes}=1024" << std::endl
-            << " -v {verbosity}=3 (info)" << std::endl
-            << " -p {print memory usage}=0" << std::endl
-            << " -i {Import forward tracks dumped from Brunel}" << std::endl
-            << " --device {select cuda device to use}=0" << std::endl
-            << " --cpu-offload {offload part of the computation to CPU}=0" << std::endl;
+/**
+ * @brief Options accepted in the program.
+ * @details Each argument is composed of the following:
+ * 
+ *          options: vector of strings with all names to arguments.
+ *          description: description of arguments, shown in print_usage.
+ *          default_value [optional]: default value the argument takes.
+ */
+struct ProgramOption {
+  std::vector<std::string> options;
+  std::string description;
+  std::string default_value = "";
+
+  ProgramOption() = default;
+  ProgramOption(const std::vector<std::string>&& options, const std::string&& description)
+    : options(options), description(description) {}
+  ProgramOption(const std::vector<std::string>&& options,
+    const std::string&& description, const std::string&& default_value)
+    : options(options), description(description), default_value(default_value) {}
+};
+
+/**
+ * @brief Prints usage of the application according to program options.
+ */
+void print_usage(char* argv[], const std::vector<ProgramOption>& program_options) {
+  std::cerr << "Usage: " << argv[0] << std::endl;
+  for (const auto& po : program_options) {
+    std::cerr << " ";
+    for (int i=0; i<po.options.size(); ++i) {
+      if (po.options[i].length() > 1) {
+        std::cerr << "-";
+      }
+      std::cerr << "-" << po.options[i];
+      if (i != po.options.size() - 1) {
+        std::cerr << ", ";
+      }
+    }
+    std::cerr << " {" << po.description << "}";
+    if (po.default_value != "") {
+      std::cerr << "=" << po.default_value;
+    }
+    std::cerr << std::endl;
+  }
+  std::cerr << " -h {show this help}" << std::endl;
 }
 
-int main(int argc, char* argv[])
-{
-  int use_mdf = 0;
-  int cuda_device = 0;
-  int cpu_offload = 1;
-  struct option long_options[] = {/* These options set a flag. */
-                                  {"mdf", no_argument, &use_mdf, 1},
-                                  {"device", required_argument, &cuda_device, 0},
-                                  {"cpu-offload", required_argument, &cpu_offload, 1},
-                                  /* These options don’t set a flag.
-                                     We distinguish them by their indices. */
-                                  {0, 0, 0, 0}};
-  // getopt_long stores the option index here.
-  int option_index = 0;
+int main(int argc, char* argv[]) {
+  // Vector of accepted options
+  // Format: options {short / long, short / long, ...}, description, default value
+  std::vector<ProgramOption> program_options {
+    {{"f", "folder"}, "folder containing data directories", "\"../input/minbias/\""},
+    {{"g", "geometry"}, "folder containing detector configuration", ""},
+    {{"mdf"}, "use MDF files as input instead of binary files", ""},
+    {{"n", "number-of-events"}, "number of events to process", "0 (all)"},
+    // {{"o", "offset"}, "offset of events from which to start", "0 (beginning)"},
+    {{"t", "threads"}, "number of threads / streams", "1"},
+    {{"r", "repetitions"}, "number of repetitions per thread / stream", "1"},
+    {{"c", "validate"}, "run validation / checkers", "1"},
+    {{"m", "memory"}, "memory to reserve per thread / stream (megabytes)", "1024"},
+    {{"v", "verbosity"}, "verbosity [0-5]", "3 (info)"},
+    {{"p", "print-memory"}, "print memory usage", "0"},
+    {{"i", "import-tracks"}, "import forward tracks dumped from Brunel", ""},
+    {{"device"}, "select cuda device to use", "0"},
+    {{"cpu-offload"}, "offload part of the computation to CPU", "0"}
+  };
 
-  // Set only the detector configuration default folder here; the rest
-  // of the defaults remains in one place in Allen.cpp
-  std::map<std::string, std::string> options{{"g", "../input/detector_configuration/down"}};
+  // Options object that will be passed to Allen
+  std::map<std::string, std::string> allen_options;
 
-  signed char c;
-  while ((c = getopt_long(argc, argv, "f:i:n:o:t:r:phd:v:c:m:g:", long_options, &option_index)) != -1) {
-    switch (c) {
-    case 0:
-      if (long_options[option_index].flag != 0) {
-        if (strncmp(long_options[option_index].name, "device", 6) == 0 && optarg) {
-          options["cuda_device"] = optarg;
-        }
-        else if (strncmp(long_options[option_index].name, "cpu-offload", 11) == 0 && optarg) {
-          options["cpu-offload"] = optarg;
-        }
-        break;
+  // Create long_options from program_options
+  std::vector<option> long_options;
+  std::string accepted_single_letter_options = "h";
+  for (const auto& po : program_options) {
+    for (const auto& opt : po.options) {
+      if (opt.length() > 1) {
+        long_options.push_back(option{opt.c_str(), required_argument, nullptr, 0});
+      } else {
+        accepted_single_letter_options += opt + ":";
       }
-      /* If this option set a flag, do nothing else now. */
-      break;
-    case 'c':
-    case 'f':
-    case 'g':
-    case 'i':
-    case 'm':
-    case 'n':
-    case 'o':
-    case 'p':
-    case 'r':
-    case 't':
-    case 'v':
-      options[std::string{c}] = optarg;
-      break;
-    case '?':
-    case 'h':
-    default:
-      printUsage(argv);
-      return -1;
     }
   }
-  options["mdf"] = std::to_string(use_mdf);
 
-  Allen::NonEventData::Updater updater{options};
+  int option_index = 0;
+  signed char c;
+  while ((c = getopt_long(argc, argv, accepted_single_letter_options.c_str(), long_options.data(), &option_index)) != -1) {
+    switch (c) {
+    case 0:
+      for (const auto& po : program_options) {
+        for (const auto& opt : po.options) {
+          if (std::string(long_options[option_index].name) == opt) {
+            if (optarg) {
+              allen_options[opt] = optarg;
+            } else {
+              allen_options[opt] = "1";
+            }
+          }
+        }
+      }
+      break;
+    default:
+      bool found_opt = false;
+      for (const auto& po : program_options) {
+        for (const auto& opt : po.options) {
+          if (std::string{c} == opt) {
+            if (optarg) {
+              allen_options[std::string{c}] = optarg;
+            } else {
+              allen_options[std::string{c}] = "1";
+            }
+            found_opt = true;
+          }
+        }
+      }
+      if (!found_opt) {
+        // If we reach this point, it is not supported
+        print_usage(argv, program_options);
+        return -1;
+      }
+      break;
+    }
+  }
 
-  return allen(std::move(options), &updater);
+  Allen::NonEventData::Updater updater{allen_options};
+  return allen(std::move(allen_options), &updater);
 }
