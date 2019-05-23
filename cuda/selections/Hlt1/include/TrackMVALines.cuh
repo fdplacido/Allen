5292f6ccda62434fff1ce6692d7019cf8fdc81d0
Thomas Boettcher <tboettch@mit.edu>
2019-05-23 11:07:45 -0400

    Started using SystemOfUnits in track mva lines.

--- cuda/selections/Hlt1/include/TrackMVALines.cuh
+++ cuda/selections/Hlt1/include/TrackMVALines.cuh
@@ -4,27 +4,28 @@
 #include "ParKalmanMath.cuh"
 #include "PV_Definitions.cuh"
 #include "VertexDefinitions.cuh"
+#include "SystemOfUnits.h"
 
 namespace TrackMVALines {
 
   // One track parameters.
   const float maxChi2Ndof = 10000.0; // Large for now until we better understand the parameterized Kalman fit quality.
-  const float minPt = 1000.0;
-  const float maxPt = 25000.0;
-  const float minIPChi2 = 7.4;
-  const float param1 = 1.0;
-  const float param2 = 1.0;
-  const float param3 = 1.1;
-  const float alpha = 2000.0;
+  const float minPt = 1000.0f / Gaudi::Units::GeV;
+  const float maxPt = 25000.0f / Gaudi::Units::GeV;
+  const float minIPChi2 = 7.4f;
+  const float param1 = 1.0f;
+  const float param2 = 1.0f;
+  const float param3 = 1.1f;
+  const float alpha = 2000.0f;
 
   // Two track parameters.
-  const float minComboPt = 2000.0;
-  const float maxVertexChi2 = 10.0;
-  const float minMCor = 1000.0;
-  const float minEta = 2.0;
-  const float maxEta = 5.0;
+  const float minComboPt = 2000.0f / Gaudi::Units::MeV;
+  const float maxVertexChi2 = 10.0f;
+  const float minMCor = 1000.0f / Gaudi::Units::MeV;
+  const float minEta = 2.0f;
+  const float maxEta = 5.0f;
   const int maxNTrksAssoc = 2; // Placeholder. To be replaced with MVA selection.
-  const float minFDChi2 = 0.0; // Placeholder. To be replaced with MVA selection.
+  const float minFDChi2 = 0.0f; // Placeholder. To be replaced with MVA selection.
 
   // Selections.
   __device__ bool OneTrackMVA(const ParKalmanFilter::FittedTrack& track);
--- cuda/selections/Hlt1/src/TrackMVALines.cu
+++ cuda/selections/Hlt1/src/TrackMVALines.cu
@@ -4,11 +4,11 @@ namespace TrackMVALines {
 
   __device__ bool OneTrackMVA(const ParKalmanFilter::FittedTrack& track)
   {
-    float ptShift = track.pt() - alpha;
+    float ptShift = (track.pt() - alpha) / Gaudi::Units::GeV;
     bool decision =  track.chi2/track.ndof < maxChi2Ndof;
     decision &= ((ptShift > maxPt && track.ipChi2 > minIPChi2) ||
                (ptShift > minPt && ptShift < maxPt &&
-                std::log(track.ipChi2) > param1 / (ptShift/1000. - param2) / (ptShift/1000. - param2)
+                std::log(track.ipChi2) > param1 / (ptShift - param2) / (ptShift - param2)
                 + param3 / maxPt * (maxPt - ptShift) + std::log(minIPChi2)));
     return decision;
   }
