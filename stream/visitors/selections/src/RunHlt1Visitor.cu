3a9dd215a59fca028ead87ecc0bc72516a06c8be
Daniel Campora <dcampora@cern.ch>
2019-05-29 11:02:18 +0200

    Less threads setting in run_hlt1_t.

--- stream/visitors/selections/src/RunHlt1Visitor.cu
+++ stream/visitors/selections/src/RunHlt1Visitor.cu
@@ -25,7 +25,7 @@ void SequenceVisitor::visit<run_hlt1_t>(
   cudaStream_t& cuda_stream,
   cudaEvent_t& cuda_generic_event)
 {
-  state.set_opts(dim3(host_buffers.host_number_of_selected_events[0]), dim3(1024), cuda_stream);
+  state.set_opts(dim3(host_buffers.host_number_of_selected_events[0]), dim3(256), cuda_stream);
   state.set_arguments(
     arguments.offset<dev_kf_tracks>(),
     arguments.offset<dev_secondary_vertices>(),
