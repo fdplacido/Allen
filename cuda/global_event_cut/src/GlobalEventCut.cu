8a6271308d96f26e6e9c341212f0a121e0ea5641
Daniel Campora <dcampora@cern.ch>
2019-06-19 20:15:20 +0200

    Removed pointless comparison of uint.

--- cuda/global_event_cut/src/GlobalEventCut.cu
+++ cuda/global_event_cut/src/GlobalEventCut.cu
@@ -45,9 +45,9 @@ __global__ void global_event_cut(
   __syncthreads();
 
   const auto num_combined_clusters = n_UT_clusters + n_SciFi_clusters;
-  //  || num_combined_clusters < min_scifi_ut_clusters
+  
   if (num_combined_clusters >= max_scifi_ut_clusters) return;
-  if (num_combined_clusters < min_scifi_ut_clusters) return;
+  // if (num_combined_clusters < min_scifi_ut_clusters) return;
 
   // passed cut
   if (threadIdx.x == 0) {
