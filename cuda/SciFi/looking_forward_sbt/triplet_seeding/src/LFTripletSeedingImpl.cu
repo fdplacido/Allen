a10b71b5767621054fee22679ac1e3394193441e
Daniel Campora <dcampora@cern.ch>
2019-08-30 15:09:33 +0200

    Added loop for more blocks in scifi lf triplet.

--- cuda/SciFi/looking_forward_sbt/triplet_seeding/src/LFTripletSeedingImpl.cu
+++ cuda/SciFi/looking_forward_sbt/triplet_seeding/src/LFTripletSeedingImpl.cu
@@ -149,9 +149,11 @@ __device__ void lf_triplet_seeding_impl(
             }
           }
 
-          if (best_k != -1) {
+          if (best_k != -1 && best_chi2 < best_combined[h1_rel + side * LookingForward::maximum_number_of_candidates].chi2) {
             best_combined[h1_rel + side * LookingForward::maximum_number_of_candidates] =
-              SciFi::CombinedValue {best_chi2, (int16_t)(best_k / LookingForward::tile_size), (int16_t)(best_k % LookingForward::tile_size)};
+              SciFi::CombinedValue {best_chi2,
+                (int16_t)(i * LookingForward::tile_size + (best_k / LookingForward::tile_size)),
+                (int16_t)(j * LookingForward::tile_size + (best_k % LookingForward::tile_size))};
           }
         }
       }
@@ -159,57 +161,65 @@ __device__ void lf_triplet_seeding_impl(
   }
 
 #else
-  // Search best triplets per h1
-  for (int k = threadIdx.x; k < LookingForward::tile_size * LookingForward::tile_size; k += blockDim.x) {
-    const int8_t h0_rel = k % LookingForward::tile_size;
-    const int8_t h2_rel = k / LookingForward::tile_size;
+  // Tiled processing of h0 and h2
+  for (int8_t i = 0; i<(h0_candidate_size + LookingForward::tile_size - 1)>> LookingForward::tile_size_shift_div; ++i) {
+    for (int8_t j = 0; j<(h2_candidate_size + LookingForward::tile_size - 1)>> LookingForward::tile_size_shift_div;
+         ++j) {
+      // Search best triplets per h1
+      for (int k = threadIdx.x; k < LookingForward::tile_size * LookingForward::tile_size; k += blockDim.x) {
+        const int8_t h0_rel = i * LookingForward::tile_size + (k % LookingForward::tile_size);
+        const int8_t h2_rel = j * LookingForward::tile_size + (k / LookingForward::tile_size);
 
-    float partial_chi2 = 10000.f * LookingForward::chi2_max_triplet_single;
-    if (h0_rel < h0_candidate_size && h2_rel < h2_candidate_size) {
-      const auto x0 =
-        scifi_hits_x0[scifi_lf_candidates[layer_0 * LookingForward::maximum_number_of_candidates + h0_rel]];
-      const auto x2 =
-        scifi_hits_x0[scifi_lf_candidates[layer_2 * LookingForward::maximum_number_of_candidates + h2_rel]];
-      partial_chi2 = x2 - x0 + x0 * zdiff - extrap2;
-    }
-    shared_partial_chi2[k] = partial_chi2;
-  }
-
-  __syncthreads();
-
-  // Iterate over all h1s
-  // Find best chi2, h0 and h2 using the partial chi2 from before
-  for (int h1_all_threads = threadIdx.x; h1_all_threads < LookingForward::maximum_number_of_triplets_per_h1 *
-                                                            LookingForward::maximum_number_of_candidates;
-       h1_all_threads += blockDim.x) {
-    const auto h1_rel = h1_all_threads % LookingForward::maximum_number_of_candidates;
-
-    if (h1_rel < h1_candidate_size) {
-      const auto side = h1_all_threads / LookingForward::maximum_number_of_candidates;
-
-      const float x1_zdiff =
-        scifi_hits_x0[scifi_lf_candidates[layer_1 * LookingForward::maximum_number_of_candidates + h1_rel]] * zdiff;
-
-      float best_chi2 = LookingForward::chi2_max_triplet_single;
-      int best_k = -1;
-
-      for (int k = 0 + side * LookingForward::tile_size * LookingForward::tile_size / 2;
-           k < LookingForward::tile_size * LookingForward::tile_size / 2 +
-                 side * LookingForward::tile_size * LookingForward::tile_size / 2;
-           ++k) {
-
-        float chi2 = shared_partial_chi2[k] - x1_zdiff;
-        chi2 = extrap1 + chi2 * chi2;
-
-        if (chi2 < best_chi2) {
-          best_chi2 = chi2;
-          best_k = k;
+        float partial_chi2 = 10000.f * LookingForward::chi2_max_triplet_single;
+        if (h0_rel < h0_candidate_size && h2_rel < h2_candidate_size) {
+          const auto x0 =
+            scifi_hits_x0[scifi_lf_candidates[layer_0 * LookingForward::maximum_number_of_candidates + h0_rel]];
+          const auto x2 =
+            scifi_hits_x0[scifi_lf_candidates[layer_2 * LookingForward::maximum_number_of_candidates + h2_rel]];
+          partial_chi2 = x2 - x0 + x0 * zdiff - extrap2;
         }
+        shared_partial_chi2[k] = partial_chi2;
       }
 
-      if (best_k != -1) {
-        best_combined[h1_rel + side * LookingForward::maximum_number_of_candidates] = SciFi::CombinedValue {
-          best_chi2, (int16_t)(best_k % LookingForward::tile_size), (int16_t)(best_k / LookingForward::tile_size)};
+      __syncthreads();
+
+      // Iterate over all h1s
+      // Find best chi2, h0 and h2 using the partial chi2 from before
+      for (int h1_all_threads = threadIdx.x; h1_all_threads < LookingForward::maximum_number_of_triplets_per_h1 *
+                                                                LookingForward::maximum_number_of_candidates;
+           h1_all_threads += blockDim.x) {
+        const auto h1_rel = h1_all_threads % LookingForward::maximum_number_of_candidates;
+
+        if (h1_rel < h1_candidate_size) {
+          const auto side = h1_all_threads / LookingForward::maximum_number_of_candidates;
+
+          const float x1_zdiff =
+            scifi_hits_x0[scifi_lf_candidates[layer_1 * LookingForward::maximum_number_of_candidates + h1_rel]] * zdiff;
+
+          float best_chi2 = LookingForward::chi2_max_triplet_single;
+          int best_k = -1;
+
+          for (int k = 0 + side * LookingForward::tile_size * LookingForward::tile_size / 2;
+               k < LookingForward::tile_size * LookingForward::tile_size / 2 +
+                     side * LookingForward::tile_size * LookingForward::tile_size / 2;
+               ++k) {
+
+            float chi2 = shared_partial_chi2[k] - x1_zdiff;
+            chi2 = extrap1 + chi2 * chi2;
+
+            if (chi2 < best_chi2) {
+              best_chi2 = chi2;
+              best_k = k;
+            }
+          }
+
+          if (best_k != -1 && best_chi2 < best_combined[h1_rel + side * LookingForward::maximum_number_of_candidates].chi2) {
+            best_combined[h1_rel + side * LookingForward::maximum_number_of_candidates] = SciFi::CombinedValue {
+              best_chi2,
+              (int16_t)(i * LookingForward::tile_size + (best_k % LookingForward::tile_size)),
+              (int16_t)(j * LookingForward::tile_size + (best_k / LookingForward::tile_size))};
+          }
+        }
       }
     }
   }
