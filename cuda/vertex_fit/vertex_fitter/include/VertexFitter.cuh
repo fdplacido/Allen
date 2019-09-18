6f172621aa10c63f3df696b3705d347968e28f9c
Thomas Boettcher <tboettch@mit.edu>
2019-09-18 18:00:20 -0400

    Added vertex fitter with no kalman filter.

--- cuda/vertex_fit/vertex_fitter/include/VertexFitter.cuh
+++ cuda/vertex_fit/vertex_fitter/include/VertexFitter.cuh
@@ -5,14 +5,20 @@
 #include "VertexDefinitions.cuh"
 #include "PV_Definitions.cuh"
 #include "SciFiConsolidated.cuh"
+#include "UTConsolidated.cuh"
+#include "VeloConsolidated.cuh"
 #include "AssociateConsolidated.cuh"
+#include "States.cuh"
 
 #include "Handler.cuh"
+#include "ArgumentsVelo.cuh"
+#include "ArgumentsUT.cuh"
 #include "ArgumentsSciFi.cuh"
 #include "ArgumentsVertex.cuh"
 #include "ArgumentsKalmanFilter.cuh"
 #include "ArgumentsPV.cuh"
 #include "ArgumentsSelections.cuh"
+#include "ArgumentsMuon.cuh"
 
 namespace VertexFit {
 
@@ -85,6 +91,31 @@ __global__ void fit_secondary_vertices(
   uint* dev_sv_offsets,
   VertexFit::TrackMVAVertex* dev_secondary_vertices);
 
+__global__ void fit_secondary_vertices_velo(
+  ParKalmanFilter::FittedTrack* dev_kf_tracks,
+  int* dev_atomics_storage,
+  uint* dev_velo_track_hit_number,
+  char* dev_velo_track_hits,
+  int* dev_atomics_veloUT,
+  uint* dev_ut_track_hit_number,
+  char* dev_ut_consolidated_hits,
+  float* dev_ut_qop,
+  uint* dev_velo_indices,
+  int* dev_n_scifi_tracks,
+  uint* dev_scifi_track_hit_number,
+  char* dev_scifi_consolidated_hits,
+  float* dev_scifi_qop,
+  MiniState* dev_scifi_states,
+  uint* dev_ut_indices,
+  char* dev_velo_kalman_beamline_states,
+  PV::Vertex* dev_multi_fit_vertices,
+  uint* dev_number_of_multi_fit_vertices,
+  char* dev_velo_pv_ip,
+  uint* dev_sv_offsets,
+  VertexFit::TrackMVAVertex* dev_secondary_vertices,
+  const bool* dev_is_muon);
+
+
 ALGORITHM(
   fit_secondary_vertices,
   fit_secondary_vertices_t,
@@ -101,3 +132,30 @@ ALGORITHM(
     dev_kalman_pv_ipchi2,
     dev_sv_offsets,
     dev_secondary_vertices))
+
+ALGORITHM(
+  fit_secondary_vertices_velo,
+  fit_secondary_vertices_velo_t,
+  ARGUMENTS(
+    dev_kf_tracks,
+    dev_atomics_velo,
+    dev_velo_track_hit_number,
+    dev_velo_track_hits,
+    dev_atomics_ut,
+    dev_ut_track_hit_number,
+    dev_ut_track_hits,
+    dev_ut_qop,
+    dev_ut_track_velo_indices,
+    dev_atomics_scifi,
+    dev_scifi_track_hit_number,
+    dev_scifi_track_hits,
+    dev_scifi_qop,
+    dev_scifi_states,
+    dev_scifi_track_ut_indices,
+    dev_velo_kalman_beamline_states,
+    dev_multi_fit_vertices,
+    dev_number_of_multi_fit_vertices,
+    dev_velo_pv_ip,
+    dev_sv_offsets,
+    dev_secondary_vertices,
+    dev_is_muon))
